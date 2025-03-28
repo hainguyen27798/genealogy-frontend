4419a55532e07b0970780d744ddf358f7016ea8a
harry Nguyen <hainguyen27798@gmail.com>
2025-03-29 05:05:41 +0700

    setup tailwindcss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import type { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('@app/modules/home/home-page/home-page.component').then((c) => c.HomePageComponent),
+  },
+];

8078072f4e923a20c504f719b9fcb20c744b3ce3
Hai Nguyen <hainguyen27798@gmail.com>
2025-05-25 19:12:30 +0700

    fix Lucide icon

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,17 +1,27 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { Eye, EyeOff, LucideAngularModule } from 'lucide-angular';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { NzInputDirective } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-login',
-  imports: [NgOptimizedImage, NzInputDirective, NzButtonComponent, TranslatePipe],
+  imports: [
+    NgOptimizedImage,
+    NzInputDirective,
+    NzButtonComponent,
+    TranslatePipe,
+    LucideAngularModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   showPassword = signal<boolean>(false);
+
+  protected Eye = Eye;
+  protected EyeOff = EyeOff;
 
   toggleShowPassword() {
     this.showPassword.set(!this.showPassword());
